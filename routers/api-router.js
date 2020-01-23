b9a0c3dc80b97bee0c55ee52088042ef0cd1fc78
keirbetts <keirjbetts@gmail.com>
2020-01-23 16:47:51 +0000

    pushed

--- routers/api-router.js
+++ routers/api-router.js
@@ -1,11 +1,13 @@
 const apiRouter = require("express").Router();
+const express = require("express");
 const topicsRouter = require("../routers/topics-router");
 const usersRouter = require("../routers/users-router");
 const articlesRouter = require("../routers/articles-router");
 const commentsRouter = require("../routers/comments-router");
-const apiInfoController = require("../controllers/api-info-controller");
+//const apiInfoController = require("../controllers/api-info-controller");
+const apiInfo2 = require("../api-info");
 
-apiRouter.route("/").get(apiInfoController);
+apiRouter.get("/", (req, res, next) => res.json({ msg: apiInfo2 }));
 apiRouter.use("/topics", topicsRouter);
 apiRouter.use("/users", usersRouter);
 apiRouter.use("/articles", articlesRouter);
--- spec/app.spec.js
+++ spec/app.spec.js
@@ -22,12 +22,31 @@ describe("/api", () => {
     });
   });
   describe("GET", () => {
-    it.only("Status: 200 responds with a JSON describing all available endpoints on the API", () => {
+    it("Status: 200 responds with a JSON describing all available endpoints on the API", () => {
       return request(app)
         .get("/api")
         .expect(200)
         .then(({ body }) => {
-          expect(body).to.eql({});
+          expect(body.msg.apiInfo2).to.eql({
+            "/api/topics":
+              "responds with an array of topic objects with the properties slug and description",
+            "api/users/:username":
+              "responds with a user object with the properties username, name and avatae_url",
+            "/api/articles":
+              "responds with an array of article objects with the correct properties and a comment_count",
+            "/api/articles/:article_id":
+              "responds with an article object with the correct properties and a comment_count",
+            "api/articles/:article_id, PATCH":
+              "Updates the votes property on the article object",
+            "api/articles/:article_id/comments POST":
+              "posts an object with the properties username and body",
+            "api/articles/:article_id/comments GET":
+              "responds with an array of comments with the correct properties, accepts the correct queries",
+            "/api/comments/:comment_id PATCH":
+              "Updates the votes property on the comments object",
+            "/api/comments/:comment_id DELETE":
+              "Deletes the given comment by the id"
+          });
         });
     });
   });
